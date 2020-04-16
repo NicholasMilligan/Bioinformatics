a7a725b97deee57c7e01860e20661c335effc3e5
Nico <nicholasrmilligan@gmail.com>
2020-04-16 03:59:24 -0700

    project 3

--- Problem_3/ComplementStrand.py
+++ Problem_3/ComplementStrand.py
@@ -4,9 +4,24 @@ class ComplementStrand (str):
     """
     def complement(self, DNAstr):
         '''Reverse the string and complement the nucleotides.'''
+        #AAAACCCGGT
+        #ACCGGGTTTT
+        comp = ""
+        for i in DNAstr[::-1]:
+            if i == 'A':
+                comp+='T'
+            elif i == 'T':
+                comp+='A'
+            elif i == 'C':
+                comp+='G'
+            elif i == 'G':
+                comp+='C'
+        print (comp)
 
 
 def main():
-    
+    DNASeq = input('Input DNA Sequence: ')
+    newComp= ComplementStrand()
+    newComp.complement(DNASeq)
 
 main()
